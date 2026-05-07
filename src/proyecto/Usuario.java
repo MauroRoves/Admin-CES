1f1738c07724c17c5d601cddf20f257d5db0c25b
Mauro <mauro.roves@gmail.com>
2026-05-07 20:52:16 -0300

    se agrearon credenciales y metodo main

--- src/proyecto/Usuario.java
+++ src/proyecto/Usuario.java
@@ -1,13 +1,19 @@
 package proyecto;
 
 public class Usuario {
+    public static void main(String[] args) {
 
-    String nombre;
-    String apellido;
-    String email;
-    String contrasena;
-    String pais;
 
+
+    String nombre = "Mauro";
+    String apellido = "Roves";
+    String email = "mauro@gmail.com";
+    String contrasena = "12345";
+    String pais = "Uruguay";
+
+    System.out.println(nombre);
+
+    }
 
 }
 
